1227ace1ed9895880e22c51baad66e71e2c1691b
ichinonayuta1 <ichinonayuta1@localhost>
2024-07-07 13:36:26 +0900

    first commit

---
+++ School_Idle_Master/src/test/java/com/example/demo/SchoolIdleMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolIdleMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
